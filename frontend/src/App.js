607cd18388425f1b245388ffa53b602bfa7b2631
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-03-09 15:26:30 +0100

    added header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
-  return <div className="App">Click2Buy</div>;
+  return (
+    <div className="App">
+      <Header />
+      Click2Buy
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
