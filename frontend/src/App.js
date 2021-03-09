b492931b189b9ed9bf738bd12e66aa0dcbfa0c1c
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-03-09 15:18:41 +0100

    removed unnecessary details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Click2Buy</div>;
 }
 
 export default App;
