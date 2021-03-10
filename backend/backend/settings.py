75df9fe7cfd9b6da7680e616cc4629e94d59c164
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-03-10 17:39:28 +0100

    linked api server to frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'corsheaders',
     
     'click2buy.apps.Click2BuyConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-
-import products from "../Product";
+import axios from "axios";
 import Product from "../components/Product";
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchProducts() {
+      const { data } = await axios.get('http://127.0.0.1:8000/api/products/')
+      setProducts(data)
+    }
+
+    fetchProducts()
+
+  }, []);
+
   return (
     <div>
       <h1>Latest Product</h1>
