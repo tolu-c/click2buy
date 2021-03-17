d803042117ae0507095ddbc46f5e3e037439c436
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-03-17 16:26:45 +0100

    register user
    in the backend with rest_framework

--- backend/click2buy/urls.py
+++ backend/click2buy/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
-    path('', views.getRoute, name='routes'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>/', views.getProduct, name='product'),
     path('users/profile/', views.getUserProfile, name='user-profile'),
     path('users/', views.getUsers, name='users'),
+    path('users/register/', views.registerUser, name='register'),
 ]
--- backend/click2buy/views.py
+++ backend/click2buy/views.py
@@ -11,38 +11,26 @@ from .serializer import ProductSerializer, UserSerializer, UserSerializerWithTok
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
 
 # Create your views here.
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-     def validate(self, attrs):
+    def validate(self, attrs):
         data = super().validate(attrs)
-        
+
         serializer = UserSerializerWithToken(self.user).data
-        
+
         for k, v in serializer.items():
             data[k] = v
-        
+
         return data
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-
-
-@api_view(['GET'])
-def getRoute(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
-        '/api/products/upload/',
-        '/api/products/<id>/reviews/',
-        '/api/products/top/',
-        '/api/products/<id>/',
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>/',
-    ]
-    return Response(routes)
 
 
 @api_view(['GET'])
@@ -80,3 +68,20 @@ def getUsers(request):
 #     users = User.objects.all()
 #     serializer = UserSerializer(users, many=True)
 #     return Response(serializer.data)
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['username'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'Warning': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
