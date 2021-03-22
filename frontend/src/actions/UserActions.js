bd7d4cf1f1a7aa591ecbd699d8ec0f01dd6d5cab
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-03-22 15:16:55 +0100

    Update UserActions.js
    formatted code

--- frontend/src/actions/UserActions.js
+++ frontend/src/actions/UserActions.js
@@ -8,35 +8,35 @@ import axios from "axios";
 
 export const login = (email, password) => async (dispatch) => {
   try {
-      dispatch({
-          type: USER_LOGIN_REQUEST
-      })
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
 
-      const config = {
-          headers: {
-              'Content-type': 'application/json'
-          }
-      }
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
 
-      const { data } = await axios.post(
-          '/api/users/login/',
-          { 'username': email, 'password': password },
-          config
-      )
+    const { data } = await axios.post(
+      "/api/users/login/",
+      { username: email, password: password },
+      config
+    );
 
-      dispatch({
-          type: USER_LOGIN_SUCCESS,
-          payload: data
-      })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
 
-      localStorage.setItem('userInfo', JSON.stringify(data))
-
+    localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
-      dispatch({
-          type: USER_LOGIN_FAIL,
-          payload: error.response && error.response.data.detail
-              ? error.response.data.detail
-              : error.message,
-      })
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
   }
-}
+};
