86696ab932bad29197ed668c4c2b9ae7f9eae0db
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-06-20 02:34:14 +0100

    updated settings

--- backend/settings.py
+++ backend/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "click2buy.herokuapp.com"]
-
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1']
 
 # Application definition
 
