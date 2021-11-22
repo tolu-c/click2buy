20d366650d221c2b992e3b3c2d099ce11c9c298a
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-11-22 10:55:15 +0100

    used postgress

--- backend/settings.py
+++ backend/settings.py
@@ -100,10 +100,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'click2buy',
+        'USER': 'admin',
+        'PASSWORD': 'asdf:lkj',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
