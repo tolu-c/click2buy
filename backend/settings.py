d328d47a911c4ab52698a0a4b2d048a74cac9963
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-11-22 11:11:49 +0100

    back to sqlite

--- backend/settings.py
+++ backend/settings.py
@@ -100,23 +100,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'click2buy',
-        'USER': 'admin',
-        'PASSWORD': 'asdf:lkj',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'click2buy',
+#         'USER': 'admin',
+#         'PASSWORD': 'asdf:lkj',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
